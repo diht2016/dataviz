def7f09233fd3b65a2c9d86fab94222fd339a4a7
diht2016 <diht2016@yandex.ru>
2021-03-12 19:59:23 +0300

    add graphml parser

---
+++ js/graphml.js
@@ -0,0 +1,15 @@
+
+function parseGraphML(xmlText) {
+    let xml = new DOMParser().parseFromString(xmlText, 'text/xml')
+    let graphElem = xml.getElementsByTagName('graph')[0]
+    let children = Array.from(graphElem.children)
+    let edgeType = graphElem.getAttribute('edgedefault')
+    let vertexIds = children.filter(e => e.tagName == 'node').map(e => e.id)
+    let graph = new Graph(vertexIds.length, isDirected = edgeType == 'directed')
+    children.filter(e => e.tagName == 'edge').forEach(e => {
+        let a = vertexIds.indexOf(e.getAttribute('source'))
+        let b = vertexIds.indexOf(e.getAttribute('target'))
+        graph.setEdge(a, b)
+    })
+    return graph
+}
