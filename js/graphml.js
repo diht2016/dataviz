7d2831ad5afa174e44bc96ecd4194a0ab7c2f3ea
diht2016 <diht2016@yandex.ru>
2021-03-15 16:33:36 +0300

    add graphml serialization

--- js/graphml.js
+++ js/graphml.js
@@ -14,3 +14,23 @@ export function parseGraphML(xmlText) {
     })
     return graph
 }
+
+export function serializeGraphML(graph) {
+    let body = []
+    graph.iterVertices(a => {
+        body.push(`<node id="n${a}"/>`)
+    })
+    graph.iterEdges((a, b) => {
+        body.push(`<edge source="n${a}" target="n${b}"/>`)
+    })
+    // todo: support directed graphs
+    return `<?xml version="1.0" encoding="UTF-8"?>
+    <graphml xmlns="http://graphml.graphdrawing.org/xmlns"  
+      xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+      xsi:schemaLocation="http://graphml.graphdrawing.org/xmlns
+      http://graphml.graphdrawing.org/xmlns/1.0/graphml.xsd">
+      <graph id="G" edgedefault="undirected">${
+          body.map(s => '\n' + ' '.repeat(8) + s).join('')}
+      </graph>
+    </graphml>`.replace(/\n {4}/g, '\n').trim()
+}
