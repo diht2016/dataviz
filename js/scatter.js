4d9e4123e3d022780cd9cc2dfffa9ec1a0a23f0f
diht2016 <diht2016@yandex.ru>
2021-04-11 15:42:40 +0300

    refactor scatter functions

--- js/scatter.js
+++ js/scatter.js
@@ -1,15 +1,19 @@
 import {range} from './shortcuts.js'
 
-export function scattered(graph) {
+export function scatter(graph) {
     graph.coords = range(graph.n, _ => [Math.random(), Math.random()])
     return graph
 }
 
-export function scatteredCircle(graph) {
-    graph.coords = range(graph.n, _ => {
+function generateCircleCoords(n) {
+    return range(n, _ => {
         let phi = Math.random() * 2 * Math.PI
         let r = Math.sqrt(Math.random()) * 0.5
         return [0.5 + r * Math.cos(phi), 0.5 + r * Math.sin(phi)]
     })
+}
+
+export function scatterCircle(graph) {
+    graph.coords = generateCircleCoords(graph.n)
     return graph
 }
